fffe12585624c1aa25942350cfdc890d65f0224f
Matheus Andrei <Matheus_Andrei@plansul.com>
2025-05-22 10:44:31 -0300

    jabaredo

--- brutos/routes/web.php
+++ brutos/routes/web.php
@@ -2,13 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web'])->prefix(trim(env('PREFIX', ''), '/'))->group(function () {
+// Route::middleware(['web'])->prefix(trim(env('PREFIX', ''), '/'))->group(function () {
+  
+
+//teste 
 
 
 
+Route::middleware(['web'])
+    ->prefix(ltrim(env('PREFIX', ''), '/'))
+    ->group(function () {
+        Route::get('/home', function () {
+            return view('welcome');
+        });
+    });
 
-Route::get('/home', function () {
-    return view('welcome');
-});
 
-});
+?>
