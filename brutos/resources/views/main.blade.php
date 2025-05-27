ad472e6888f63e22a4e196187052dbe43993805a
Noan Brostt <calielnoan@gmail.com>
2025-05-27 16:00:41 -0300

    Revert "Contornando problema rota do asset"
    This reverts commit e1c5591d7cbe74f97619df7623c72cc3e8d89f6b.

--- brutos/resources/views/main.blade.php
+++ brutos/resources/views/main.blade.php
@@ -6,25 +6,24 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Estilos -->
-        <link rel="stylesheet" href="../css/font-roboto.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/font-roboto.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/cropper.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/sweetalert2.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/app.css">
+        <link rel="stylesheet" href="{{ asset('css/font-roboto.css') }}" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/cropper.min.css') }}" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/sweetalert2.min.css') }}" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="../js/sweetalert2.min.js"></script>
-        <script src="../js/fontawesome.min.js"></script>
-        <script src="../js/jquery-3.7.1.min.js"></script>
-        <script src="../js/cropper.min.js"></script>
-        <script src="../js/dataTables.min.js"></script>
-        <script src="../js/bootstrap-4.0.0.min.js"></script>
-        <script src="../js/jquery.mask-1.14.15.min.js"></script>
-        <script src="../js/hammer.min.js"></script>
-        <script src="../js/app.js"></script>
+        <script src="{{ asset('js/sweetalert2.min.js') }}"></script>
+        <script src="{{ asset('js/fontawesome.min.js') }}"></script>
+        <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+        <script src="{{ asset('js/cropper.min.js') }}"></script>
+        <script src="{{ asset('js/dataTables.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap-4.0.0.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.mask-1.14.15.min.js') }}"></script>
+        <script src="{{ asset('js/hammer.min.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
 
-        <link rel="icon" type="image/x-icon" href="../img/favicon.jpg">
+        <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.jpg') }}">
         <title>@yield('title', 'Plansul')</title>
     </head>
 
