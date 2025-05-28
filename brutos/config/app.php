a7b2b12e56f48b4c8e04a49acfab52ad61f6c3fa
Noan Brostt <calielnoan@gmail.com>
2025-05-28 11:04:10 -0300

    url e asset_url

--- brutos/config/app.php
+++ brutos/config/app.php
@@ -52,7 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/playground7'),
+
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
