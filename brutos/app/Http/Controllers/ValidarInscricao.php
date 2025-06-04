0264e216c43ac96f60d5aafccafeabdd73223374
Matheus Andrei <Matheus_Andrei@plansul.com>
2025-06-04 14:29:03 -0300

    Ajuste consulta listar validação

--- brutos/app/Http/Controllers/ValidarInscricao.php
+++ brutos/app/Http/Controllers/ValidarInscricao.php
@@ -40,14 +40,16 @@ class ValidarInscricao extends Controller{
         $inscricoes = DB::connection('tinder2')
             ->table('public.usuario as u')
             ->join('public.tipo_intencao as ti', 'u.id_tipo_intencao', '=', 'ti.id_tipo_intencao')
+            ->join('public.status_usuario as st', 'st.id_status_usuario', '=', 'u.id_status_usuario')
             ->select(
                 'u.id_usuario',
                 'u.matricula',
                 'u.nome',
                 'ti.no_tipo_intencao as intencao',
-                'u.de_sobre as sobre'
-            )
+                'st.no_status_usuario as status_usr',
+                'u.de_sobre as sobre' )
             ->get();
+
     
         return response()->json($inscricoes);
     }
