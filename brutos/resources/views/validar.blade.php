a70b1a1606e12e16fd22e826bd1b2e988b59bb8b
Noan Brostt <calielnoan@gmail.com>
2025-06-12 09:58:51 -0300

    Tela de validar com botão para ir pro Tinder

--- brutos/resources/views/validar.blade.php
+++ brutos/resources/views/validar.blade.php
@@ -383,10 +383,16 @@
 
     }
 
-    #redirectInscricao {
+    .redirect {
         display: flex;
         align-items: center;
         gap: 10px;
+        width: 100%;
+        justify-content: space-between;
+
+        &:not(:first-child) {
+            margin-top: 10px;
+        }
     }
 
     @media screen and (max-width: 640px) {
@@ -420,17 +426,31 @@
         <img src="{{ asset('img/logo-tinder-white.png') }}" alt="Logo" />
     </span>
     <h1 style="text-align: center;">Validação de Candidatos</h1>
-    <button id="redirectInscricao" onClick="window.location.href = '/inscricao';">
-        Tela de Inscrição
-        <svg xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            width="22"
-            height="22"
-            fill="currentColor"
-        >
-            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-        </svg>
-    </button>
+    <span>
+        <button class="redirect" onClick="window.location.href = '/inscricao';">
+            Tela de Inscrição
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                width="22"
+                height="22"
+                fill="currentColor"
+            >
+                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+            </svg>
+        </button>
+
+        <button class="redirect" onClick="window.location.href = '/tinder';">
+            Tela do Tinder
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                width="22"
+                height="22"
+                fill="currentColor"
+            >
+                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+            </svg>
+        </button>
+    </span>
 </div>
 
 <div id="contador"></div>
